2ec87321ecbdd12fdbfbd196a1d9c998400cfd8b
saloni180 <saloni180@gmail.com>
2026-04-06 17:55:45 +0530

    Updates to variables

--- Main.tf
+++ Main.tf
@@ -32,12 +32,12 @@ provider "azurerm" {
 
   resource "azurerm_resource_group" "rg" {
     name     = "rg-terraform"
-    location = "eastus"
+    location = var.location
   }
 
   resource "azurerm_storage_account" "sa" {
     name                     = "sa-terraform"
-    location                 = "eastus"
+    location                 = var.location
     resource_group_name      = azurerm_resource_group.rg.name
     account_tier             = "Standard"
     account_replication_type = "LRS"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "location"{
+    description ="Azure region"
+    type="string"
+    default="eastus"
+}
+
+variable "account tier"{
+    description = "account tier"    
+    type="string"
+    default="Standard"
+}
